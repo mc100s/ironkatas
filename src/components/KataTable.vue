64bb9f70cfdaa3d7d88491c627f11e5ad04feab8
Maxence Bouret <maxence.bouret@gmail.com>
2017-11-10 15:27:40 +0100

    Update of KataTable and data

--- src/components/KataTable.vue
+++ src/components/KataTable.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
+  <div class="flex-center">
     <span class="is-hidden"> {{ value }}</span>
     <table class="table">
       <thead>
         <tr>
           <th>Katas</th>
+          <th>Info</th>
           <th v-for="student in students"><abbr :title="student.firstname">{{ student.codewarsPseudo }}</abbr></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="kata in katas.filter(kata => !kata.other)">
           <td><a :href="'https://www.codewars.com/kata/' + kata.slug">{{ kata.slug }}</a></td>
-          <td v-for="student in students">
+          <td class="has-text-centered"><button v-if="getKataRank(kata)" :class="'button is-small'">{{ getKataRank(kata) }}</button></td>
+          <td v-for="student in students" class="has-text-centered">
             <button class="button is-primary is-small" v-if="student.katasSolved && student.katasSolved.map(kata => kata.slug).indexOf(kata.slug) !== -1">
               <i class="fa fa-check"></i>
             </button>
@@ -24,10 +26,12 @@
         </tr>
         <tr class="is">
           <th>Other Katas</th>
+          <th></th>
           <th v-for="student in students"></th>
         </tr>
         <tr v-for="kata in katas.filter(kata => kata.other)">
           <td><a :href="'https://www.codewars.com/kata/' + kata.slug">{{ kata.slug }}</a></td>
+          <td><button v-if="getKataRank(kata)" :class="'button is-small'">{{ getKataRank(kata) }}</button></td>
           <td v-for="student in students">
             <button class="button is-primary is-small" v-if="student.katasSolved && student.katasSolved.map(kata => kata.slug).indexOf(kata.slug) !== -1">
               <i class="fa fa-check"></i>
@@ -49,33 +53,68 @@ export default {
     return {
       students: [
         {
-          firstname: "Maxence",
-          codewarsPseudo: "mc100s",
-        },
-        // {
-        //   firstname: "Charlotte",
-        //   codewarsPseudo: "Charlotte",
-        // },
-        {
           firstname: "Antoine",
           codewarsPseudo: "amarc27",
         },
         {
-          firstname: "Vivian",
-          codewarsPseudo: "VivianSolide",
+          firstname: "Charlotte",
+          codewarsPseudo: "charlottebtmn",
         },
         {
           firstname: "David",
           codewarsPseudo: "dmc1985",
         },
+        {
+          firstname: "Julien",
+          codewarsPseudo: "jb1404"
+        },
+        {
+          firstname: "Maxence",
+          codewarsPseudo: "mc100s",
+        },
+        {
+          firstname: "Medhi",
+          codewarsPseudo: "Medhi"
+        },
+        {
+          firstname: "Vivian",
+          codewarsPseudo: "VivianSolide",
+        },
       ],
       katas: [
-        {slug: "multiply"},
-        {slug: "hello-w-dot-dot-dot-wait-what"},
-        {slug: "largest-product-in-a-series-1"},
-        {slug: "largest-product-in-a-series-2"}
+        {slug: "multiply", rank: "8 (bugs)"},
+        {slug: "even-or-odd", rank: "8 (maths)"},
+        {slug: "youre-a-square", rank: "7 (maths)"},
+        {slug: "jaden-casing-strings", rank: "7 (strings)"},
+        {slug: "sort-array-by-string-length", rank: "7 (sort)"},
+        {slug: "pete-the-baker", rank: "7 (objects)"},
+        {slug: "powers-of-3", rank: "7 (maths)"},
+        {slug: "folding-your-way-to-the-moon", rank: "7 (maths)"},
+        {slug: "reversed-strings", rank: "7 (strings)"},
+        {slug: "multiples-of-3-or-5", rank: "6 (maths)"},
+        {slug: "write-number-in-expanded-form", rank: "6 (maths)"},
+        {slug: "custom-array-dot-concat-method", rank: "6 (arrays)"},
+        {slug: "scramblies", rank: "5 (strings)"},
+        {slug: "moving-zeros-to-the-end", rank: "5 (arrays)"},
+        {slug: "valid-parentheses", rank: "5 (strings)"},
+        {slug: "hello-w-dot-dot-dot-wait-what", rank: "4 (puzzle)"},
+        {slug: "largest-product-in-a-series-1", rank: "(maths)"},
       ],
       errorMessage: ""
+    }
+  },
+  methods: {
+    getKataRank(kata) {
+      if (!kata.rank) {
+        // axios.get(`https://www.codewars.com/api/v1/code-challenges/${kata.slug}`)
+        //   .then((response) => {
+        //     kata.rank = response.data.rank.name
+        //     kata.color = response.data.rank.color
+        //     this.$emit('input', Math.random());
+        //   })
+
+      }
+      return kata.rank;
     }
   },
   watch: {
@@ -137,5 +176,10 @@ li {
 
 a {
   color: #42b983;
+}
+
+.flex-center{
+  display: flex;
+  justify-content: center;
 }
 </style>
